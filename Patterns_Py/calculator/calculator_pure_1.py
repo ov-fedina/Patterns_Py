2ce3ff4891a8733f899534571486bb64d13d926f
Olga Fedina <langust85@gmail.com>
2022-02-23 20:31:50 +0300

    Create calculator drafts

---
+++ Patterns_Py/calculator/calculator_pure_1.py
@@ -0,0 +1,25 @@
+print ('Welcome to Calculator!')
+q1 = int (input('Enter number 1: '))
+q2 = int (input('Enter number 2: '))
+
+v = int (input('What operation do you want to perform? \n 1 Addition \n 2 Subtraction \n 3 Division \n 4 Multiplication \n'))
+
+if v == 1:
+    r = q1 + q2
+    p = 'Addition'
+    t = p
+if v == 2:
+    r = q1 - q2
+    l = 'Subtraction'
+    t = l
+if v == 3:
+    r = float(q1 / q2)
+    m = 'Division'
+    t = m
+if v == 4:
+    r = q1 * q2
+    n = 'Multiplication'
+    t = n
+print ('Result ',t,' = ',r)
+
+
---
+++ Patterns_Py/calculator/calculator_pure_2.py
@@ -0,0 +1,72 @@
+def welcome():
+    print('''
+Welcome to Calculator!
+''')
+
+welcome() # Don’t forget to call the function
+
+
+# Define our function
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+** for power
+% for modulo
+''')
+# Don’t forget to add more conditional statements to solve for power and modulo
+
+
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+    # Add again() function to calculate() function
+    again()
+
+
+
+def again():
+
+    # Take input from user
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    # If user types Y, run the calculate() function
+    if calc_again.upper() == 'Y':
+        calculate()
+
+    # If user types N, say good-bye to the user and end the program
+    elif calc_again.upper() == 'N':
+        print('See you later!')
+
+    # If user types another key, run the function again
+    else:
+        again()
+
+# Call calculate() outside of the function
+calculate()
